f0ca8b82cacf9131d17e4db89adaa3da4551e7da
Achira Fernando <achirafernando97@gmail.com>
2021-06-03 09:50:36 +0530

    Adding comments

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+    // This is a fully reusable component that contains a ng-content which can
+    // render content that is wrapped between the app-card selector.
+    // This way we can reuse the app-card to make your own cards in the application.
+
+    // Right now we wrap movie-detail template as it's the only place we use cards.
     constructor() { }
 
     ngOnInit(): void {
